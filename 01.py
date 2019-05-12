958134d044793ddf36c3414b77ce09d8933c08c2
xwp <xwp_fullstack@163.com>
2019-05-12 16:40:54 +0800

    del

--- 01.py
+++ 01.py
@@ -1,11 +1,3 @@
 print("hello world")
 print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
 
